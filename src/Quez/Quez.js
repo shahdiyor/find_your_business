a23c9946b8679530401b9a3826d880768a2b4848
Shahdiyor Munavvarov <shakhdiyorm@gmail.com>
2022-05-15 12:41:26 +0700

    create quize module

---
+++ src/Quez/Quez.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./Quez.css";
+
+class Quez extends Component {
+    render() {
+        return (
+            <div className='mainQ'>
+                
+            </div>
+        );
+    }
+}
+
+export default Quez;
